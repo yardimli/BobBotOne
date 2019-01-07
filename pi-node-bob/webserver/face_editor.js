c0d2caf129de8bda84b054b82427ff8ecbf53a8a
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2019-01-07 21:36:14 +0800

    integrating face editor to the UI

--- pi-node-bob/webserver/face_editor.js
+++ pi-node-bob/webserver/face_editor.js
@@ -1,6 +1,10 @@
 //---------------------------------------------------------------------------------------------------------------------------
 
 
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+
 var mini_canvas = document.getElementById("mini_canvas");
 var mini_context = mini_canvas.getContext("2d");
 var DivideMiniScreen = 2.5;
@@ -10,6 +14,12 @@ var bh = window.innerHeight;
 var p = 0;
 
 var Highlights = [];
+var MouseCurrentX = 0;
+var MouseCurrentY = 0;
+var CurrentColor = "blue";
+var LastMouseCurrentX = 0;
+var LastMouseCurrentY = 0;
+var SaveHighlights = [];
 
 var PlayAnimationSeq = [];
 var PlayAnimationRow = 0;
@@ -39,26 +49,61 @@ function LoadFaceFile(face_name) {
 function drawLED(face_name) {
   var grd;
 
+
   for (var y = 0; y <= 8 * 40; y += 40) {
     for (var x = 0; x <= 16 * 40; x += 40) {
-      grd = mini_context.createLinearGradient(0.5 + (x / DivideMiniScreen), 0.5 + (y / DivideMiniScreen), 0.5 + (x / DivideMiniScreen) + (40 / DivideMiniScreen), 0.5 + (y / DivideMiniScreen) + (40 / DivideMiniScreen));
+      grd = context.createLinearGradient(0.5 + x, 0.5 + y, 0.5 + x + 40, 0.5 + y + 40);
+
+      if ((y / 40) === MouseCurrentY && (x / 40) === MouseCurrentX) {
+        grd.addColorStop(0, "red");
+        grd.addColorStop(1, "#333");
+      }
+      else {
+        grd.addColorStop(0, "#000");
+        grd.addColorStop(1, "#333");
+      }
+
+      context.fillStyle = grd;
+      context.fillRect(0.5 + x, 0.5 + y, 40, 40);
+
+      grd = context.createLinearGradient(0.5 + (x / DivideMiniScreen), 0.5 + (y / DivideMiniScreen), 0.5 + (x / DivideMiniScreen) + (40 / DivideMiniScreen), 0.5 + (y / DivideMiniScreen) + (40 / DivideMiniScreen));
       grd.addColorStop(0, "#000");
       grd.addColorStop(1, "#222");
       mini_context.fillStyle = grd;
       mini_context.fillRect(0.5 + (x / DivideMiniScreen), 0.5 + (y / DivideMiniScreen), 40 / DivideMiniScreen, 40 / DivideMiniScreen);
+
+
     }
   }
 
 
   for (var i = 0; i < Highlights.length; i++) {
+
+    grd = context.createLinearGradient(
+      0.5 + (Highlights[i].X * 40),
+      0.5 + (Highlights[i].Y * 40),
+      0.5 + 40 + (Highlights[i].X * 40),
+      0.5 + 40 + (Highlights[i].Y * 40));
+
     var DrawColor = "Yellow";
     if (typeof Highlights[i].Color === "undefined") {
+
     }
     else {
       DrawColor = Highlights[i].Color;
     }
 
-    grd = mini_context.createLinearGradient(
+    grd.addColorStop(0, DrawColor);
+    grd.addColorStop(1, "#333");
+
+    context.fillStyle = grd;
+    context.fillRect(
+      0.5 + (Highlights[i].X * 40),
+      0.5 + (Highlights[i].Y * 40),
+      40,
+      40);
+
+    grd = context.createLinearGradient(
       0.5 + (Highlights[i].X * (40 / DivideMiniScreen)),
       0.5 + (Highlights[i].Y * (40 / DivideMiniScreen)),
       0.5 + (40 / DivideMiniScreen) + (Highlights[i].X * (40 / DivideMiniScreen)),
@@ -73,11 +118,36 @@ function drawLED(face_name) {
       (40 / (DivideMiniScreen + 2)),
       (40 / (DivideMiniScreen + 2)));
   }
-
   $.get("/robot_cmd?set_face=yes&face_name=" + face_name, function (data, status) {
 //        console.log("Data: " + data + "    -- Status: " + status);
   });
+}
 
+//---------------------------------------------------------------------------------------------------------------------------
+function drawBoard() {
+
+  for (var y = 0; y <= 16 * 40; y += 40) {
+    context.moveTo(0.5 + y + p, p);
+    context.lineTo(0.5 + y + p, bh + p);
+  }
+
+
+  for (var x = 0; x <= 8 * 40; x += 40) {
+    context.moveTo(p, 0.5 + x + p);
+    context.lineTo(bw + p, 0.5 + x + p);
+  }
+
+  context.strokeStyle = "black";
+  context.stroke();
+}
+
+//---------------------------------------------------------------------------------------------------------------------------
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
 }
 
 
@@ -86,30 +156,20 @@ $(document).ready(function () {
   //---------------------------------------------------------------------------------------------------------------------------
   bw = 40 * 16;//window.innerWidth;
   bh = 40 * 8;// window.innerHeight;
+  canvas.width = bw;
+  canvas.height = bh;
+
+
   mini_canvas.width = bw / DivideMiniScreen;
   mini_canvas.height = bh / DivideMiniScreen;
 
-//    LoadFaceFile("face1.txt");
-
   PlayAnimationSeq = [];
-  PlayAnimationSeq.push({"FileName": "face1", "Delay": 50});
-  PlayAnimationSeq.push({"FileName": "face2", "Delay": 50});
-  PlayAnimationSeq.push({"FileName": "face3", "Delay": 50});
-  PlayAnimationSeq.push({"FileName": "face4", "Delay": 50});
-  PlayAnimationSeq.push({"FileName": "face5", "Delay": 50});
-  PlayAnimationSeq.push({"FileName": "face6", "Delay": 50});
-  PlayAnimationSeq.push({"FileName": "face7", "Delay": 50});
-  PlayAnimationSeq.push({"FileName": "face8", "Delay": 50});
-  PlayAnimationSeq.push({"FileName": "face9", "Delay": 50});
-  PlayAnimationSeq.push({"FileName": "face10", "Delay": 50});
-  PlayAnimationSeq.push({"FileName": "face11", "Delay": 50});
-  PlayAnimationSeq.push({"FileName": "face12", "Delay": 50});
-  PlayAnimationSeq.push({"FileName": "face13", "Delay": 50});
-  PlayAnimationSeq.push({"FileName": "face14", "Delay": 50});
   PlayAnimationRow = -1;
   PlayAnimationIntCounter = 0;
   PlayAnimation = false;
 
+  drawLED();
+  drawBoard();
 
   setInterval(function () {
     if (PlayAnimation && PlayAnimationSeq.length > 0) {
@@ -135,7 +195,7 @@ $(document).ready(function () {
 
 
   UrlToGet = "/robot_cmd";
-  var data = {"load_faces":"yes"};
+  var data = {"load_faces": "yes"};
 
   $.ajax({
     url: UrlToGet,
@@ -159,7 +219,7 @@ $(document).ready(function () {
   });
 
   UrlToGet = "/robot_cmd";
-  var data = {"load_face_animations":"yes"};
+  var data = {"load_face_animations": "yes"};
 
   $.ajax({
     url: UrlToGet,
@@ -183,6 +243,13 @@ $(document).ready(function () {
   });
 
   //---------------------------------------------------------------------------------------------------------------------------
+  $(".color_menu").on('click', function (e) {
+    CurrentColor = $(this).data("color");
+    e.preventDefault();
+  });
+
+
+  //---------------------------------------------------------------------------------------------------------------------------
   $("#face_list").on('change', function (e) {
     console.log("load face");
     PlayAnimation = false;
@@ -195,7 +262,7 @@ $(document).ready(function () {
     console.log("load animation");
 
     UrlToGet = "/robot_cmd";
-    var data = {"load_face_animation":"yes","face_animation_name": $(this).val() + ".txt"};
+    var data = {"load_face_animation": "yes", "face_animation_name": $(this).val() + ".txt"};
 
     $.ajax({
       url: UrlToGet,
@@ -210,7 +277,7 @@ $(document).ready(function () {
         for (var i = 0; i < lines.length; i++) {
           var linex = lines[i];
           linex = linex.split(";");
-          if (linex.length==2) {
+          if (linex.length == 2) {
             PlayAnimationSeq.push({"FileName": linex[0].trim(), "Delay": linex[1].trim()});
           }
 
@@ -227,5 +294,61 @@ $(document).ready(function () {
 
     e.preventDefault();
   });
-})
-;
+
+
+  setTimeout(function () {
+    drawLED();
+    drawBoard();
+  }, 2000);
+
+  canvas.addEventListener('mousemove', function (evt) {
+    var mousePos = getMousePos(canvas, evt);
+
+    MouseCurrentX = Math.floor(mousePos.x / 40);
+    MouseCurrentY = Math.floor(mousePos.y / 40);
+
+    if (MouseCurrentX >= 16) {
+      MouseCurrentX = 16;
+    }
+    if (MouseCurrentY >= 8) {
+      MouseCurrentY = 8;
+    }
+
+    if (MouseCurrentX !== LastMouseCurrentX || MouseCurrentY !== LastMouseCurrentY) {
+      LastMouseCurrentX = MouseCurrentX;
+      LastMouseCurrentY = MouseCurrentY
+      var message = 'Mouse position: ' + MouseCurrentX + ',' + MouseCurrentY;
+
+      drawLED();
+      drawBoard();
+
+//        writeMessage(canvas, message);
+//        writeMessage(canvas, "##MYIP##");
+    }
+  }, false);
+
+
+  canvas.addEventListener('click', function (event) {
+//	console.log("click");
+
+
+    PlayAnimation = false;
+    var DeleteDot = false;
+    for (var i = 0; i < Highlights.length; i++) {
+      if (Highlights[i].X == MouseCurrentX && Highlights[i].Y == MouseCurrentY) {
+        Highlights.splice(i, 1);
+        DeleteDot = true;
+        break;
+      }
+    }
+
+    if (!DeleteDot) {
+      Highlights.push({X: MouseCurrentX, Y: MouseCurrentY, Color: CurrentColor});
+    }
+
+    drawLED();
+    drawBoard();
+  }, false);
+
+
+});
